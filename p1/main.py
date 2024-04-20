69d8e8dd36196a59a4d84e46d74b8b70ba5fc198
edmharx <edmharxx@gmail.com>
2024-04-20 18:13:06 +0800

    creation of function for the separation of integers

--- p1/main.py
+++ p1/main.py
@@ -2,3 +2,15 @@
 #creation of the files
 #read lines in number.txt
 #per line write the number to designated text file
+
+def read_write():
+    with open("numbers.txt", "r") as source_file:
+        for line in source_file:
+            if int(line) % 2 == 0:
+                with open("even.txt", "a") as even_output_file:
+                    even_output_file.write(line)
+            else:
+                with open("odd.txt", "a") as odd_output_file:
+                    odd_output_file.write(line)
+
+read_write()
