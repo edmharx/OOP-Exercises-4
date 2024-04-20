b64698e2e56914fdb0ce26f5e22fb5a8f2ff2d93
edmharx <edmharxx@gmail.com>
2024-04-20 19:40:24 +0800

    read the dictionary of datas in students.txt

--- p2/main.py
+++ p2/main.py
@@ -2,3 +2,11 @@
 #compare all the gwa(values)
 #print the name(keys) and gwa(values) of the student with the highest gwa
 
+directory = (r"students.txt")
+def highest_gwa(directory):
+    with open(directory, "r") as source_file:
+        data = source_file.read()
+        data_dict = eval(data)
+
+
+highest_gwa(directory)
