1b9effa3a23d241fc11c84f0268f670830d6779c
edmharx <edmharxx@gmail.com>
2024-04-20 18:16:01 +0800

    explanation

--- p1/main.py
+++ p1/main.py
@@ -5,8 +5,8 @@
 
 def read_write():
     with open("numbers.txt", "r") as source_file:
-        for line in source_file:
-            if int(line) % 2 == 0:
+        for line in source_file: #loops reading of line
+            if int(line) % 2 == 0: #check if integer per line is divisible by 2
                 with open("even.txt", "a") as even_output_file:
                     even_output_file.write(line)
             else:
