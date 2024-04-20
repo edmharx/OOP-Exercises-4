0ac9a9ae2f6a7e015088f9e432eafa996f95d2d4
edmharx <edmharxx@gmail.com>
2024-04-20 19:48:17 +0800

    determining the highest grade

--- p2/main.py
+++ p2/main.py
@@ -7,6 +7,12 @@ def highest_gwa(directory):
     with open(directory, "r") as source_file:
         data = source_file.read()
         data_dict = eval(data)
+        highest_grade = None
+        for value in data_dict.values():
+            if highest_grade is None or value > highest_grade:
+                highest_grade = value
+        print(f"")
+
 
 
 highest_gwa(directory)
