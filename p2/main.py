5f261b744e70db621cb116e390ef51902e8ff611
edmharx <edmharxx@gmail.com>
2024-04-20 20:12:26 +0800

    printing the student with the highest grade and the grade

--- p2/main.py
+++ p2/main.py
@@ -3,16 +3,18 @@
 #print the name(keys) and gwa(values) of the student with the highest gwa
 
 directory = (r"students.txt")
-def highest_gwa(directory):
+def find_highest_gwa(directory):
     with open(directory, "r") as source_file:
         data = source_file.read()
         data_dict = eval(data)
-        highest_grade = None
-        for value in data_dict.values():
-            if highest_grade is None or value > highest_grade:
-                highest_grade = value
-        print(f"")
+        highest_gwa = None
+        highest_gwa_student = None
+        for key, value in data_dict.items():
+            if highest_gwa is None or value > highest_gwa:
+                highest_gwa_student = key
+                highest_gwa = value
+        return highest_gwa_student, highest_gwa
 
 
-
-highest_gwa(directory)
+key, value = find_highest_gwa(directory)
+print (f"{key} has the highest GWA with with {value}")
