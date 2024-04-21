ef6f8f0b2053730c5581ddc886d4574533143d22
Edmharx Macasinag <edmharxx@gmail.com>
2024-04-21 10:06:18 +0800

    add another loop for invalid answer

--- p3/main.py
+++ p3/main.py
@@ -1,20 +1,25 @@
 #open the output file
 #create a funtion to append a user input to the output file
-#integrate the function
+#loop if there are more input
+#break if there are no more input
+#loop to input question when invalid answer
 
 def input_line():
-    while True:
+    resume = True
+    while resume:
         line = input("Enter line: ")
         enter_line = line+("\n") #puts the new input on another line
         with open("mylife.txt", "a") as source_file:
             source_file.write(enter_line)
-            again = input("Are there more lines y/n? ")
-            if again.lower() == "y":
-                continue
-            elif again.lower() == "n":
-                break
-            else:
-                print("Invalid answer!")
-                break
+            while resume:
+                again = input("Are there more lines y/n? ")
+                if again.lower() == "y":
+                    break
+                elif again.lower() == "n":
+                    print ("Sure!")
+                    resume = False #breaks the two while loops
+                else:
+                    print("Invalid answer! Please type y/n only.")
+                    continue
 
 input_line()
