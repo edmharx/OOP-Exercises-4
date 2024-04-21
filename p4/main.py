f15e4542eb6f4eb7777dbe09275816f77dd0843a
Edmharx Macasinag <edmharxx@gmail.com>
2024-04-21 10:37:46 +0800

    converts all line as integers and create new line per number

--- p4/main.py
+++ p4/main.py
@@ -6,12 +6,14 @@
 def square_cube():
     with open("integers.txt", "r") as source_file:
         for line in source_file:
-            if int(line) % 2 == 0: #for even numbers
+            line = int(line)
+            if line % 2 == 0: #for even numbers
                 square_root = line ** 2
                 with open("double.txt", "a") as squared_output:
-                    squared_output.write(square_root)
+                    squared_output.write(str(square_root)+"\n")
             else:
                 cube_root = line ** 3 #for odd numbers
                 with open("triple.txt", "a") as cube_output:
-                    cube_output.write(cube_root)
-            
+                    cube_output.write(str(cube_root)+"\n")
+
+square_cube()
