719f0d93340e2a7febc907d66b6f545dc220889c
Edmharx Macasinag <edmharxx@gmail.com>
2024-04-21 09:19:41 +0800

    function to append user input

--- p3/main.py
+++ p3/main.py
@@ -2,3 +2,17 @@
 #create a funtion to append a user input to the output file
 #integrate the function
 
+def input_line():
+    while True:
+        line = input("Enter line: ")
+        with open("mylife.txt", "a") as source_file:
+            source_file.write(line)
+            again = input("Are there more lines y/n? ")
+            if again.lower == "y":
+                continue
+            elif again.lower =="n":
+                break
+            else:
+                print("Invalid answer!")
+                break
+                
