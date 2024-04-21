84b06a639489f10ef4c65c1c7ae92a363c8ce19b
Edmharx Macasinag <edmharxx@gmail.com>
2024-04-21 09:33:03 +0800

    new line

--- p3/main.py
+++ p3/main.py
@@ -5,14 +5,16 @@
 def input_line():
     while True:
         line = input("Enter line: ")
+        enter_line = line+("\n") #puts the new input on another line
         with open("mylife.txt", "a") as source_file:
-            source_file.write(line)
+            source_file.write(enter_line)
             again = input("Are there more lines y/n? ")
-            if again.lower == "y":
+            if again.lower() == "y":
                 continue
-            elif again.lower =="n":
+            elif again.lower() == "n":
                 break
             else:
                 print("Invalid answer!")
                 break
-                
+
+input_line()
