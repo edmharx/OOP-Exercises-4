8c1800bcbf45abee92546fda8a356cf8f191ee6a
Edmharx Macasinag <edmharxx@gmail.com>
2024-04-21 10:21:11 +0800

    even numbers

---
+++ p4/main.py
@@ -0,0 +1,13 @@
+#create source txt file
+#create two functions to apply for the integers
+#input the results into their respective txt files
+
+
+def square_cube():
+    with open("integers.txt", "r") as source_file:
+        for line in source_file:
+            if int(line) % 2 == 0: #for even numbers
+                square_root = line ** 2
+                with open("double.txt", "a") as squared_output:
+                    squared_output.write(square_root)
+            
